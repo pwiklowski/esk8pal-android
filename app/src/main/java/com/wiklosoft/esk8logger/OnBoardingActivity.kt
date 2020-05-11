72674280c7a9a17ec2c5a7ed7be499bc7ad3c24a
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-05-11 21:25:51 +0200

    Ask user to enable bt before using app

--- app/src/main/java/com/wiklosoft/esk8logger/OnBoardingActivity.kt
+++ app/src/main/java/com/wiklosoft/esk8logger/OnBoardingActivity.kt
@@ -1,7 +1,10 @@
 package com.wiklosoft.esk8logger
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.wiklosoft.esk8logger.ui.loading.LoadingFragment
 
 class OnBoardingActivity : AppCompatActivity() {
@@ -15,6 +18,11 @@ class OnBoardingActivity : AppCompatActivity() {
                 .commitNow()
         }
 
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 1)
+        }
     }
 
     override fun onResume() {
